031b9e9edb30cf67dc91ece99af40fcef6be27ca
josh <104086979+juswa277353@users.noreply.github.com>
2025-08-15 13:45:29 +0800

    Update gallery to use hardcoded data and add promo button
    Replaces dynamic gallery data with a hardcoded galleryItems array in the Gallery component and adds a 'Discover Now' button linking to a promotion. Updates CSS to style the new button, adjusts link colors, image margins, and improves mobile responsiveness for the about section.

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,7 +1,16 @@
 import { Image } from "./image";
 import React from "react";
 
-export const Gallery = (props) => {
+export const Gallery = () => {
+  // Hardcoded gallery data
+  const galleryItems = [
+    {
+      largeImage: "/img/portfolio/01-large.jpg",
+      smallImage: "/img/portfolio/01-small.jpg",
+      link: "https://event.sari-sari.com/?fbclid=IwY2xjawMCMCpleHRuA2FlbQIxMABicmlkETF0cTRLb2VFd3ZLRWxTaXRYAR65KxD_gQ_1wHyd7t1O7xCBaDD6Cl4lgwjGPQD5D7_3_B_1wZe7k9Ae_923RQ_aem_5RfUuVtDyUHXDoqCcqFB6A",
+    },
+  ];
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
@@ -13,22 +22,30 @@ export const Gallery = (props) => {
         </div>
         <div className="flex justify-center">
           <div className="portfolio-items flex flex-wrap justify-center gap-4">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4 flex justify-center"
-                  >
-                    <div className="relative group w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
-                      <Image
-                        title={d.title}
-                        largeImage={d.largeImage}
-                        smallImage={d.smallImage} 
-                      />
-                    </div>
+            {galleryItems.map((d, i) => (
+              <div
+                key={`${d.title}-${i}`}
+                className="col-sm-6 col-md-4 col-lg-4 flex justify-center"
+              >
+                <div className="relative group w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                  <Image
+                    title={d.title}
+                    smallImage={d.smallImage}
+                  />
+                  <div className="btn-gallery">
+                    <a
+                      href={d.link} 
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-lg transition duration-300"
+
+                    >
+                      Discover Now
+                    </a>
                   </div>
-                ))
-              : "Loading..."}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
